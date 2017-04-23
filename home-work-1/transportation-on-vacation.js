ae4496c2cdb397b4184a0048fba8f00b3b0a5dc1
amshekar <amshekar@hotmail.com>
2017-04-23 23:58:50 +0530

    Update transportation-on-vacation.js

--- home-work-1/transportation-on-vacation.js
+++ home-work-1/transportation-on-vacation.js
@@ -1,1 +1,12 @@
 
+function rentalCarCost(d) {
+  // Your solution here
+ var days=Number(d),baseFare=40,totalRent=0;
+ if(days <3)
+  totalRent = (days*baseFare);
+ else if(days >=3 && days<7)
+  totalRent = (days*baseFare) - 20;
+ else if(days >=7)
+  totalRent = (days*baseFare)-50;  
+  return totalRent;
+}
