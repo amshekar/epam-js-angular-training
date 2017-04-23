5fd8e9f48231d5f5b14d11c558cea67190c603e9
amshekar <amshekar@hotmail.com>
2017-04-23 22:38:34 +0530

    word count initial

--- home-work-1/word-count.js
+++ home-work-1/word-count.js
@@ -1,1 +1,10 @@
 
+function countWords(str) {
+if(!str.trim())
+return 0;
+else if(str.indexOf("﻿"))
+return str.trim().split(﻿).length;
+else 
+return str.trim().split(' ').length;
+ // return result;
+}
