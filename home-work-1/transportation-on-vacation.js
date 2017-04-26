54abd820c000a5673360ff1d4da5a5ad09dba8c0
Shekar Gadamoni <Shekar_Gadamoni@epam.com>
2017-04-26 18:40:04 +0530

    Implemented the review comments

--- home-work-1/transportation-on-vacation.js
+++ home-work-1/transportation-on-vacation.js
@@ -1,14 +1,12 @@
 function rentalCarCost(days) {
     const baseFare = 40;
-    let totalRent = days * baseFare;
-    if (days > 0 && days < 3) {
-        totalRent = totalRent;
-    }
-    else if (days >= 3 && days < 7) {
-        totalRent -= 20;
+    let totalFare = days * baseFare;
+    if (days >= 3 && days < 7) {
+        totalFare -= 20;
     }
     else if (days >= 7) {
-        totalRent -= 50;
+        totalFare -= 50;
     }
     return totalRent;
 }
+
