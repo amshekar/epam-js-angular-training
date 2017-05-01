800de52e5bb7fca37a228834e2f001206ea04d9d
amshekar <amshekar@hotmail.com>
2017-05-01 20:50:59 +0530

    finished the task h-w-3

--- home-work-3/extract-nested-object-reference.js
+++ home-work-3/extract-nested-object-reference.js
@@ -1,1 +1,13 @@
-// JavaScript source code
+// return the nested property value if it exists,
+// otherwise return undefined
+Object.prototype.hash = function(string) {
+  var obj=this;
+  var arr=string.split(".");
+  arr.forEach(function(elem){
+  if(obj)
+  {
+  obj=obj[elem];
+  }
+  });
+  return obj;
+}
