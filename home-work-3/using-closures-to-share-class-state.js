104ab4bea8541f578514dbcfb8c2e49bdd560e67
amshekar <amshekar@hotmail.com>
2017-05-04 07:58:41 +0530

    Update using-closures-to-share-class-state.js

--- home-work-3/using-closures-to-share-class-state.js
+++ home-work-3/using-closures-to-share-class-state.js
@@ -1,1 +1,45 @@
-// JavaScript source code
+
+var Cat = function(name,weight) {
+var sum = 0,count=0;
+this.name=name;
+this.weight=weight; 
+sum +=this.weight;
+
+};
+Object.defineProperty(Cat,"sum",{ 
+ get:function()
+ {
+ return sum;
+ },
+ set:function()
+ {
+ sum =sum+cat.weight;
+ }
+ });
+ Object.defineProperty(Cat,"count",{ 
+ get:function()
+ {
+ return 2;
+ }
+ 
+ });
+Object.defineProperty(Cat, "averageWeight", {  
+    get: function(){
+      return function(){  
+        return count ? sum / count : 0;
+      };
+    }
+  });
+  
+
+//corrected test methods
+fluffy = new Cat('fluffy', 15);
+garfield = new Cat('garfield', 25);
+
+
+Test.assertEquals(fluffy.weight, 15);
+Test.assertEquals(fluffy instanceof Cat, true);
+Test.assertEquals(fluffy.averageWeight, undefined);
+Test.assertEquals(typeof Cat.averageWeight, "function");
+Test.assertEquals(Cat.averageWeight(), 20);
+
