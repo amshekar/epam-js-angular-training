51f250d836f336ac22339a5ff2bd519e3b2ef913
Shekar Gadamoni <Shekar_Gadamoni@epam.com>
2017-05-09 19:29:13 +0530

    implemented the review comment

--- home-work-3/using-closures-to-share-class-state.js
+++ home-work-3/using-closures-to-share-class-state.js
@@ -1,32 +1,36 @@
+// Let's make a Cat constructor!
+ var sum = 0, count=[];
+ function Cat(name, weight) { 
 
-var Cat = function (name, weight) {
-    var sum = 0, count = 0;
-    this.name = name;
-    this.weight = weight;
-    sum += this.weight;
+if (typeof name == 'undefined' || typeof weight == 'undefined') {
+         throw new Error('Cats not instantiated with name, weight: ' + name + ', ' + weight);
+     }
+  this.name = name;
+  this.weight = weight;
+  sum += this.weight;
+  count.push(this);
+ };
 
-};
 Object.defineProperty(Cat, "sum", {
-    get: function () {
-        return sum;
-    },
-    set: function () {
-        sum = sum + cat.weight;
-    }
-});
-Object.defineProperty(Cat, "count", {
-    get: function () {
-        return 2;
-    }
+     get: function () {
+         return sum;
+     },
+     set: function () {
+         sum = sum + cat.weight;
+     }
+ });
+ 
 
-});
-Object.defineProperty(Cat, "averageWeight", {
-    get: function () {
-        return function () {
-            return count ? sum / count : 0;
-        };
-    }
-});
+
+ Object.defineProperty(Cat, "averageWeight", {
+     get: function () {
+         return function () {
+             return count ? sum / count.length : 0;
+         };
+     }
+ });
+
+
 
 
 //corrected test methods
