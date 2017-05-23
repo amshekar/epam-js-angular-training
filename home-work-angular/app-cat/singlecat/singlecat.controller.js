e31f2c17ee01961be326627051666aaf8a271dff
Shekar Gadamoni <Shekar_Gadamoni@epam.com>
2017-05-23 17:21:49 +0530

    created new single app for angular

---
+++ home-work-angular/app-cat/singlecat/singlecat.controller.js
@@ -0,0 +1,17 @@
+//h-w-1 section 1
+(function (module) {
+    "use strict";
+    function SingleCatController() {
+        var vm = this;
+        vm.count = 0;
+        vm.catClick = catClick;
+
+        function catClick() {
+            vm.count++;
+        }
+
+    }
+    module.controller("SingleCatController", SingleCatController);
+
+
+})(angular.module("catviewer"));
